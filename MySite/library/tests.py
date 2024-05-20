4448665d5534e9015f35eea64eef786c64ff1b16
Kazakov Nikita <prapifka102@gmail.com>
2024-05-20 12:13:10 +0500

    Добавил тесты

--- MySite/library/tests.py
+++ MySite/library/tests.py
@@ -1,17 +1,35 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client
 from library import models
 
 
 class Test(TestCase):
-    def SetUp(self):
+    def setUp(self):
         self.client = Client()
+        self.genre = models.Genre.objects.create(name="Фэнтези")
         self.book = models.Book.objects.create(
             title="Ведьмак 1",
             author="Сапковский",
             description="Некое описание книги",
-            genre=1,
+            genre=self.genre,
             pages=120,
             price=300,
+            photo=SimpleUploadedFile("media/example.jpg", b"file_content", content_type="image/jpeg")
         )
+
+    def test_index(self):
+        response = self.client.get('/index/')
+        self.assertEquals(response.status_code, 200)
+
+
+    def test_genre(self):
+        response = self.client.get(f'/genre/{self.genre.id}/')
+        self.assertEquals(response.status_code, 200)
+
+
+    def test_book_detail(self):
+        response = self.client.get(f'/books/{self.book.id}/')
+        self.assertEquals(response.status_code, 200)
+
 
 
--- MySite/library/urls.py
+++ MySite/library/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('genre/<int:pk>', GenreView.as_view(), name='genre'),
-    path('books/', BookList.as_view(), name='genre'),
+    path('genre/<int:pk>/', GenreView.as_view(), name='genre'),
+    path('index/', BookList.as_view(), name='genre'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail')
 ]
